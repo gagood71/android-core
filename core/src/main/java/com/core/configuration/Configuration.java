9f812860139a477e98a676fa77b29f3c9c263bbc
gagood71 <gagood71@gmail.com>
2023-11-29 09:12:19 +0800

    因為 .aar 檔案內部的資源 ID 是獨立的，無法直接透過外部專案的 R 文件引用

--- core/src/main/java/com/core/configuration/Configuration.java
+++ core/src/main/java/com/core/configuration/Configuration.java
@@ -21,6 +21,23 @@ public class Configuration {
         public static final int NORMAL_BUTTON_FONT_COLOR_2 = R.color.normal_button_font_color_2;
     }
 
+    public static final class String {
+        public static final int 系統 = R.string.系統;
+        public static final int 系統初始化 = R.string.系統初始化;
+        public static final int 系統設定 = R.string.系統設定;
+        public static final int 系統提示 = R.string.系統提示;
+        public static final int 系統訊息 = R.string.系統訊息;
+        public static final int 系統記錄 = R.string.系統記錄;
+
+        public static final int 確認 = R.string.確認;
+        public static final int 取消 = R.string.取消;
+        public static final int 警告 = R.string.警告;
+        public static final int 連接 = R.string.連接;
+        public static final int 連接中 = R.string.連接中;
+        public static final int 連接成功 = R.string.連接成功;
+        public static final int 連接失敗 = R.string.連接失敗;
+    }
+
     public static final class Time {
         public static final long TIME_0 = 0;
         public static final long TIME_500 = 500;
